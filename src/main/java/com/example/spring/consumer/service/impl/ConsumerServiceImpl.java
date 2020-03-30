2231de39dc80bd8b5e0155f11be8777fda5f2a22
Igor Intercont <masterintercont@gmail.com>
2020-03-30 16:45:44 -0300

    Consumer completo, consumindo das filas configuradas no projeto Producer.

---
+++ src/main/java/com/example/spring/consumer/service/impl/ConsumerServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.spring.consumer.service.impl;
+
+import com.example.spring.consumer.dto.Message;
+import com.example.spring.consumer.service.ConsumerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerServiceImpl implements ConsumerService {
+    @Override
+    public void action(Message message) throws Exception {
+        System.out.println(message.getText());
+    }
+}
